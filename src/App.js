8d25be56c0603a129ad8c0fa3af7ef471115baca
Matthieu <matthieunadeau96@gmail.com>
2018-07-10 14:29:56 -0400

    Added the getWeather api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+
+const API_KEY = "109fc051af1ab9681cf5cade41dc33d7";
+
 class App extends Component {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=' + API_KEY);
+    const data = await api_call.json();
+    console.log(data);
+  }
+
   render() {
     return (
-      
+      <div>
+        <Titles/>
+        <Form getWeather={this.getWeather}/>
+        <Weather/>
+      </div>
     );
   }
 }
