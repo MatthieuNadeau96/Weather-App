efa9fc18dadcbb437da429c404852f2386a63987
Matthieu <matthieunadeau96@gmail.com>
2018-07-11 12:07:48 -0400

    fixed api to have city & country instead of lat & lon

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const API_KEY = "109fc051af1ab9681cf5cade41dc33d7";
 class App extends Component {
   getWeather = async (e) => {
     e.preventDefault();
-    const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=' + API_KEY);
+    const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=' + API_KEY);
     const data = await api_call.json();
     console.log(data);
   }
